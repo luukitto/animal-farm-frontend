d8d4dbeb1a45b5f45180cce108061b6d5fdb9104
luukitto <luukitto@gmail.com>
2025-02-10 03:08:44 +0400

    Use angular material style in animal component
    1. Add filtering, sorting and paging to data table
    2. Use georgian language as a default table language

--- src/app/components/animal/animal.component.ts
+++ src/app/components/animal/animal.component.ts
@@ -1,28 +1,88 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
 import { AnimalService } from "../../services/animal.service";
 import { Animal } from "../../models/animal.model";
 import { NgForOf, NgIf } from "@angular/common";
 import { PigStatusComponent } from "../pig-status/pig-status.component";
 import { Status } from "../../models/pig.model";
+import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from "@angular/material/card";
+import {
+  MatCell, MatCellDef,
+  MatColumnDef,
+  MatHeaderCell, MatHeaderCellDef,
+  MatHeaderRow,
+  MatHeaderRowDef,
+  MatRow, MatRowDef,
+  MatTable, MatTableDataSource
+} from "@angular/material/table";
+import { MatIcon } from "@angular/material/icon";
+import { MatProgressSpinner } from "@angular/material/progress-spinner";
+import { MatMiniFabButton } from "@angular/material/button";
+import { MatFormField, MatLabel } from "@angular/material/form-field";
+import { MatPaginator, MatPaginatorIntl } from "@angular/material/paginator";
+import { MatSort, MatSortHeader } from "@angular/material/sort";
+import { MatInput } from "@angular/material/input";
+import { GeorgianPaginatorIntl } from "../../localization/georgian-paginator.intl";
 
 @Component({
   selector: 'app-animal',
   standalone: true,
   imports: [
     NgIf,
-    NgForOf
+    NgForOf,
+    MatCardTitle,
+    MatCardHeader,
+    MatCard,
+    MatCardContent,
+    MatTable,
+    MatHeaderCell,
+    MatCell,
+    MatIcon,
+    MatHeaderRow,
+    MatRow,
+    MatProgressSpinner,
+    MatColumnDef,
+    MatMiniFabButton,
+    MatHeaderRowDef,
+    MatRowDef,
+    MatLabel,
+    MatSortHeader,
+    MatHeaderCellDef,
+    MatCellDef,
+    MatFormField,
+    MatPaginator,
+    MatInput,
+    MatSort
+  ],
+  providers: [
+    {provide: MatPaginatorIntl, useClass: GeorgianPaginatorIntl}
   ],
   templateUrl: './animal.component.html',
   styleUrl: './animal.component.css'
 })
-export class AnimalComponent implements OnInit {
-  animals: Animal[] = [];
+export class AnimalComponent implements OnInit, AfterViewInit {
   loading = false;
   error: string | null = null;
   @Input() pigStatusRef!: PigStatusComponent;
+  displayedColumns: string[] = ['name', 'type', 'arkipoCounter', 'actions'];
+  dataSource = new MatTableDataSource<Animal>([]);
+
+
+
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(
     private animalService: AnimalService) {
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
   ngOnInit() {
@@ -35,22 +95,19 @@ export class AnimalComponent implements OnInit {
 
     this.animalService.feedAnimal(id).subscribe({
       next: (data) => {
-        // Update Animal
-        const fedAnimal = data
-        const index = this.animals.findIndex(animal => animal.id === fedAnimal.id);
+        const index = this.dataSource.data.findIndex(animal => animal.id === data.id);
         if (index !== -1) {
-          this.animals[index] = fedAnimal;
+          const updatedData = [...this.dataSource.data];
+          updatedData[index] = data;
+          this.dataSource.data = updatedData;
         }
 
-        // Update PigStatus to HAPPY
         this.pigStatusRef.updatePigStatus(Status.HAPPY);
-
-        // Update PigStatus to DEFAULT after 3 seconds
         setTimeout(() => {
           this.pigStatusRef.updatePigStatus(Status.DEFAULT);
         }, 3000);
       }
-    })
+    });
   }
 
   getAnimals(): void {
@@ -59,7 +116,7 @@ export class AnimalComponent implements OnInit {
 
     this.animalService.getAnimals().subscribe({
       next: (data) => {
-        this.animals = data;
+        this.dataSource.data = data
         this.loading = false;
       },
       error: (error) => {
---
+++ src/app/localization/georgian-paginator.intl.ts
@@ -0,0 +1,18 @@
+import { MatPaginatorIntl } from '@angular/material/paginator';
+
+export class GeorgianPaginatorIntl extends MatPaginatorIntl {
+  override itemsPerPageLabel = 'ჩანაწერები გვერდზე:';
+  override nextPageLabel = 'შემდეგი გვერდი';
+  override previousPageLabel = 'წინა გვერდი';
+  override firstPageLabel = 'პირველი გვერდი';
+  override lastPageLabel = 'ბოლო გვერდი';
+
+  override getRangeLabel = (page: number, pageSize: number, length: number) => {
+    if (length === 0) {
+      return `0-დან 0-მდე ${length}-დან`;
+    }
+    const start = page * pageSize + 1;
+    const end = Math.min((page + 1) * pageSize, length);
+    return `${start}-დან ${end}-მდე ${length}-დან`;
+  };
+}
